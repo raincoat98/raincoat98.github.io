1fa5b8f2fa92edcdddcf237a26670c3ca5c09068
WooSangWook <raincoat@kakao.com>
2025-11-14 15:32:35 +0900

    Remove: 문서 통계 관련 코드 및 컴포넌트 제거
    - Dashboard, DocumentStats 컴포넌트 삭제
    - generateStats.js, gitStats.js 스크립트 삭제
    - package.json에서 stats 관련 스크립트 제거
    - Layout.vue에서 DocumentStats 제거
    - 폰트 설정 추가 (custom.css)

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Comment from "../components/Comment.vue";
-import DocumentStats from "../components/DocumentStats.vue";
 import DefaultTheme from "vitepress/theme";
 
 const { Layout } = DefaultTheme;
@@ -9,7 +8,6 @@ const { Layout } = DefaultTheme;
 <template>
   <Layout>
     <template #doc-footer-before>
-      <DocumentStats />
       <Comment />
     </template>
   </Layout>
